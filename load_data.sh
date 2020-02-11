725367abc95d2417c966630337724ea64e2214f3
irusri@yahoo.com <irusri@yahoo.com>
2020-02-11 10:52:59 +0100

    Loading a novel genome to genie-sys

--- load_data.sh
+++ load_data.sh
@@ -1,10 +1,15 @@
+#load_data.sh script
 #!/bin/bash
 #load_data.sh
 #USAGE: sh load_data.sh [table_name] [filename]
-#sh load_data.sh transcript_info_x /tmp/transcript_info.tsv
+#sh load_data.sh transcript_info transcript_info.txt
+
+DB_USER='your_db_username'
+DB_PASS='your_password'
+DB='database_name'
 
 /usr/bin/mysql --host=localhost --user=$DB_USER --password=$DB_PASS --local_infile=1 --database=$DB <<EOFMYSQL
 TRUNCATE TABLE $1;
 ALTER TABLE $1 AUTO_INCREMENT = 1;
-load data local infile '$2' ignore INTO TABLE $1 CHARACTER SET UTF8 fields terminated by '\t' LINES TERMINATED BY '\n' ignore 0 lines;
+load data local infile '$2' replace INTO TABLE $1 fields terminated by '\t' LINES TERMINATED BY '\n' ignore 0 lines;
 EOFMYSQL
